0990aa83809eb001279838497e452cc5904a3cc7
221B268 <221B268@juet.co.in>
2025-01-24 14:21:51 +0530

    This is the lab1

---
+++ Le1/problem4/One.java
@@ -0,0 +1,8 @@
+public class One
+{
+    int x;
+    One(int x)
+    {
+        this.x = x;
+    }
+}
---
+++ Le1/problem4/Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+    public static void main(String args[])
+    {
+        One o1 = new One(12);
+        Two t1 = new Two();
+    }
+}
+//error: constructor One in class One cannot be applied to given types;
