70e181b625006909f1d7259855bb6e4e4debea9a
221B268 <221B268@juet.co.in>
2025-01-31 14:52:43 +0530

    This is Lab2

--- Le2/Prob2/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Voice2 voice = new Voice2();
-        voice.templateMethod();
-    }
-}
--- Le2/Prob2/Voice.java
+++ Le2/Prob2/Voice.java
@@ -1,9 +1,18 @@
-public class Voice {
-    public void prepareVoice() {
-        System.out.println("Preparing voice...");
+class Voice{
+     Animal[] animal = new Animal[5];
+    
+    void prepareVoice(){
+        animal[0] = new Cow();
+        animal[1] = new Goat();
+        animal[2] = new Pig();
+        animal[3] = new Lion();
+        animal[4] = new Tiger();
+        
     }
-
-    public void hear() {
-        System.out.println("Hearing the voice...");
+    
+    void hear(Animal[] animal){
+        for(int i=0; i<5;i++){
+            animal[i].makeVoice();
+        }
     }
 }
